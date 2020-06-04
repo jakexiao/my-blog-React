e0a768e9351c19aec8bdf3ec6606e9916696cfc8
jakexiao <xiaosifan0223@hotmail.com>
2020-06-04 22:31:15 +0100

    first commit

---
+++ src/build/precache-manifest.a2c1c5fd9cc298ac5276adfc8b10f13a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4750bc9478c6d226d675b9dee9b8bd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f35d820d110e63310d2",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "2df66dbe303e73d7644b",
+    "url": "/static/js/2.b4be504e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4be504e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f35d820d110e63310d2",
+    "url": "/static/js/main.1a4747bf.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
